16f8ba04aa8a7685bb396880c83651d337441cb7
Sunil Bishwakarma <bishwast77@gmail.com>
2021-09-11 22:40:49 -0600

    Add project files.

---
+++ SBTipCalculatorApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SBTipCalculatorApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBTipCalculatorApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet] //Gets request from the client.
+        public IActionResult Index()
+        {
+            var model = new TipCalculatorModel();
+
+            //Below I am have created 4 properties for index() method if the ViewBag Property, which are auto available to the controllers and View for this App.
+            ViewBag.costOfMeal = "";
+            //Setting the FifteenPercentTip property to the decimal value to the right. 
+            ViewBag.FifteenPercentTip = 0;
+            //Setting the TwentyPercentTip property to the decimal value to the right. 
+            ViewBag.TwentyPercentTip = 0;
+
+            //Setting the TwentyFivePercentTip property to the decimal value to the right. 
+            ViewBag.TwentyFivePercentTip = 0;
+
+            //Using View() method to return a ViewResult Object for the view accociated with the Index() action method.
+            return View(model);
+        }
+        [HttpPost] //Posts response to the Index screen for client requst from the model class.
+        public IActionResult Index(TipCalculatorModel model)
+        {
+            //Validating if the data is correct. 
+            if (ModelState.IsValid)
+            {
+                //Calling the results from the custom methods from class TipCalculator and passing in to the ViewBag below.  
+                ViewBag.FifteenPercentTip = model.CalculateTips_15();
+                ViewBag.TwentyPercentTip = model.CalculateTips_20();
+                ViewBag.TwentyFivePercentTip = model.CalculateTips_25();
+            }
+            else
+            {
+                //Set the fields to zero.
+                ViewBag.FifteenPercentTip = 0;
+                ViewBag.TwentyPercentTip = 0;
+                ViewBag.TwentyFivePercentTip = 0;
+
+            }
+            //Return the model to the view for client display.
+            return View(model);
+        }
+    }
+}
---
+++ SBTipCalculatorApp/Models/TipCalculatorModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+//Here, I have used Validation attributes of the DataAnnotations namespace to add Validation rules to my model.
+
+namespace SBTipCalculatorApp.Models
+{
+    public class TipCalculatorModel
+    {
+        //This model c# class has 3 properties to get Cost of the meal and set tips for 15, 20 or 25 percent of the meal cost for this App.
+        //Making the value as required for the property.
+        //Hence, if the user doesn't enter a value for this property, the MVC framework generates a error msg.
+
+        [Required (ErrorMessage = "Please enter the total Cost of your Meal.")]
+        //Value of the property must be within the specified range of values.
+        [Range(0,100000,ErrorMessage = "The Cost of the Meal must be greater than $0.00.")]
+        public decimal? costOfMeal { get; set; }
+
+        /// <summary>
+        /// Custom method to calculate tips based on percentage.
+        /// </summary>
+        /// <returns></returns>
+        public double? CalculateTips_15()
+        {
+
+            //define variables
+            decimal? fifteenPT = 0;
+            //For loop to validate the cost of the meal and calculate the tip.
+            for (decimal i=0; i < costOfMeal; i++)
+            {
+                fifteenPT = 15 * costOfMeal / 100;
+            }
+            return (double?)fifteenPT;
+        }
+        /// <summary>
+        /// Custom method for 20 % tip calculation
+        /// </summary>
+        /// <returns></returns>
+        public decimal? CalculateTips_20()
+        {
+            //Defined the initial value of the variable to 0.
+            decimal? twentyPT = 0;
+            //For loop to validate the cost of the meal and calculate the tip.
+            for (decimal i = 0; i < costOfMeal; i++)
+            {
+                twentyPT = 20 * costOfMeal / 100;
+            }
+            return twentyPT;
+        }
+        public decimal? CalculateTips_25()
+        {
+            //Defined the initial value of the variable to 0.
+
+            decimal? twenty_5_PT = 0;
+            //For loop to validate the cost of the meal and calculate the tip.
+            for (decimal i = 0; i < costOfMeal; i++)
+            {
+                twenty_5_PT = 25 * costOfMeal / 100;
+            }
+            return twenty_5_PT;
+        }
+
+    }
+}
